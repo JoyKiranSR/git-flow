06d4469c039cf210058b87e6c39fb05c37887314
Joy Kiran S R <srjoykiran@gmail.com>
2024-06-24 04:02:37 +0530

    added signup functionality

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const { signup } = require("./auth/signup");
+
 console.log('Index working');
+
+// signup call
+console.log("Signing a new user...");
+console.log(signup({name: "Joy", email: "joy@gmail.com", pass: "1234"}));
